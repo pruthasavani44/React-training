bed15a79c3e5eba8bfd96fa9e754cdd4e08a334b
pruthasavani44 <pruthasavani.micrasol@gmail.com>
2024-09-02 18:33:06 +0530

    variable

--- react-yarn/src/App.js
+++ react-yarn/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 
 function App() {
+
+  console.log(x);
+  var x = 10;
+  console.log(x);
+
+  x = 20;
+  console.log(x);
+
+
+
+  // console.log(p);
+  let p = 10;
+  console.log(p);
+
+  p = 2000;
+  console.log(p);
+
+
+
   return (
     <>
       <h1 className='text-gray-900 text-2xl'>This is react Project using Yarn</h1>
+
+
+
+
     </>
   );
 }
